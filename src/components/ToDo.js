a55836e9055bb4cec59c91de0867c2ab794d00b7
jeongbeen2 <aceman9508@gmail.com>
2021-06-06 13:06:51 +0900

    react-redux done

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store';
+import { Link } from 'react-router-dom';
 
-function ToDo({ text, onBtnClick }) {
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      {text}
+      <Link to={`/${id}`}>{text}</Link>
       <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 }
 function mapDispatchToProps(dispatch, ownProps) {
-  console.log('ownProps;', ownProps);
+  // console.log('ownProps;', ownProps);
   return {
     onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
   };
--- src/route/Detail.js
+++ src/route/Detail.js
@@ -1,1 +1,25 @@
-export default () => 'Detail';
+import React from 'react';
+import { connect } from 'react-redux';
+// import { useParams } from 'react-router-dom';
+function Detail({ toDos }) {
+  //   const id = useParams();
+  console.log(toDos);
+  return (
+    <>
+      <h1>text : {toDos?.text}</h1>
+      <h1>Created At: {toDos?.id}</h1>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  //   console.log('ownProps:', ownProps);
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  //   console.log('id', id);
+  return { toDos: state.find((todo) => todo.id === parseInt(id)) };
+}
+export default connect(mapStateToProps)(Detail);
